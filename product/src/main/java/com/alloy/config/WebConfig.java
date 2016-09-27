02db99997032f7a2942ca48c5ae0a0e9f61cd05d
Steven Ma <7348348564@stevenma.xyz>
2016-09-27 16:18:00 -0400

    Add jquery and angular dependencies

--- product/src/main/java/com/alloy/config/WebConfig.java
+++ product/src/main/java/com/alloy/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
--- product/src/main/webapp/javascript/core.js
+++ product/src/main/webapp/javascript/core.js
@@ -2,5 +2,10 @@
  * Created by stevenma on 9/27/16.
  */
 $(document).ready(function() {
-    window.location.reload(true);
+    window.onload = function() {
+        if (!window.location.hash) {
+            window.location = window.location + '#loaded';
+            window.location.reload(true);
+        }
+    }
 });
