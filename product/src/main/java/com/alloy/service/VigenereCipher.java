106af0fc31d65cd14ed3b1f69744522a13646df6
Steven Ma <7348348564@stevenma.xyz>
2016-10-02 21:37:12 -0400

    Add getVariance

--- product/src/main/java/com/alloy/service/VigenereCipher.java
+++ product/src/main/java/com/alloy/service/VigenereCipher.java
@@ -1,6 +1,9 @@
 package com.alloy.service;
 
+import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by stevenma on 9/27/16.
@@ -8,7 +11,11 @@ import java.util.HashMap;
 public class VigenereCipher
 {
 
-    private HashMap<Character, Integer> rotationLookup = new HashMap<>();
+    private final String language = "ENGLISH";
+    private final Integer N = 26;
+    private final Double mu = 1.0/N;
+    private final HashMap<Character, Integer> rotationLookup = new HashMap<>();
+    private final HashMap<Character, Double> relativeFrequency = new HashMap<>();
 
     public VigenereCipher()
     {
@@ -16,6 +23,33 @@ public class VigenereCipher
         {
             rotationLookup.put((char) (i + 'a'), i);
         }
+
+        relativeFrequency.put('A', 0.08167);
+        relativeFrequency.put('B', 0.01492);
+        relativeFrequency.put('C', 0.02782);
+        relativeFrequency.put('D', 0.04253);
+        relativeFrequency.put('E', 0.12702);
+        relativeFrequency.put('F', 0.02228);
+        relativeFrequency.put('G', 0.02015);
+        relativeFrequency.put('H', 0.06094);
+        relativeFrequency.put('I', 0.06996);
+        relativeFrequency.put('J', 0.00153);
+        relativeFrequency.put('K', 0.00772);
+        relativeFrequency.put('L', 0.04025);
+        relativeFrequency.put('M', 0.02406);
+        relativeFrequency.put('N', 0.06749);
+        relativeFrequency.put('O', 0.07507);
+        relativeFrequency.put('P', 0.01929);
+        relativeFrequency.put('Q', 0.00095);
+        relativeFrequency.put('R', 0.05987);
+        relativeFrequency.put('S', 0.06327);
+        relativeFrequency.put('T', 0.09056);
+        relativeFrequency.put('U', 0.02758);
+        relativeFrequency.put('V', 0.00978);
+        relativeFrequency.put('W', 0.02360);
+        relativeFrequency.put('X', 0.00150);
+        relativeFrequency.put('Y', 0.01974);
+        relativeFrequency.put('Z', 0.00074);
     }
 
     public String encrypt(String plaintext, String key)
@@ -74,6 +108,36 @@ public class VigenereCipher
 
     public String crack(String ciphertext)
     {
+
         return "placeholder" + ciphertext;
     }
+
+    private Double getVariance(String message)
+    {
+        Double variance = 0.0;
+        Integer length = 0;
+
+        HashMap<Character, Integer> letterCount = new HashMap<>();
+        //ArrayList<Double> frequency = new ArrayList<>();
+
+        for (int i = 0; i < 26; ++i)
+        {
+            letterCount.put((char) (i + 'a'), 0);
+            ++length;
+        }
+
+        for (char character : message.toCharArray())
+        {
+            letterCount.replace(character, letterCount.get(character) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : letterCount.entrySet())
+        {
+            Double frequency = (double) entry.getValue()/N;
+            variance += (frequency - mu)*(frequency - mu);
+        }
+
+        return variance*mu;
+    }
+
 }
