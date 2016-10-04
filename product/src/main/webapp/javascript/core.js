705195481543826512b73b183b6c60f5cc34c1d7
Steven Ma <7348348564@stevenma.xyz>
2016-10-04 11:57:00 -0700

    Make home page default

--- product/src/main/webapp/javascript/core.js
+++ product/src/main/webapp/javascript/core.js
@@ -2,10 +2,12 @@
  * Created by stevenma on 9/27/16.
  */
 $(document).ready(function() {
-    window.onload = function() {
-        if (!window.location.hash) {
-            window.location = window.location + '#loaded';
-            window.location.reload(true);
-        }
-    }
+
+    // Open homepage
+    var loadTarget = document.getElementById("home");
+    var targetTab = document.getElementById("home-tab");
+    loadTarget.style.display = "block";
+    targetTab.className += " active";
+    $(loadTarget).load("views/home.html");
+
 });
