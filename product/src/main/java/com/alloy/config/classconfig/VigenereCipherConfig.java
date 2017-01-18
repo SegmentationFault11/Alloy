77c4624c8124cd09f38c09a4a590dde2cd5d4ce2
Steven Ma <7348348564@stevenma.xyz>
2017-01-18 10:04:36 -0500

    Add singleton declaration for Vignere

--- product/src/main/java/com/alloy/config/classconfig/VigenereCipherConfig.java
+++ product/src/main/java/com/alloy/config/classconfig/VigenereCipherConfig.java
@@ -3,6 +3,7 @@ package com.alloy.config.classconfig;
 import com.alloy.service.VigenereCipher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 /**
  * Created by stevenma on 9/27/16.
@@ -12,6 +13,7 @@ public class VigenereCipherConfig
 {
 
     @Bean
+    @Scope("singleton")
     public VigenereCipher vigenereCipher()
     {
         return new VigenereCipher();
